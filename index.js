5215f495941c5c752dc048b0ba1cb53142173dec
Adnan <77362547+adddeveloper@users.noreply.github.com>
2022-02-21 12:21:07 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,117 @@
+var url, position, holder, img;
+img = "https://www.coinlore.com/img/25x25/";
+holder = document.querySelector(".holder-in-window");
+position = [0,30];
+url = "https://api.coinlore.net/api/tickers/?start="+position[0]+"&limit="+position[1];
+/*
+csupply: "18945677.00"
+id: "90"
+market_cap_usd: "755814649764.44"
+msupply: "21000000"
+name: "Bitcoin"
+nameid: "bitcoin"
+percent_change_1h: "-0.45"
+percent_change_7d: "-5.60"
+percent_change_24h: "-0.14"
+price_btc: "1.00"
+price_usd: "39893.78"
+rank: 1
+symbol: "BTC"
+tsupply: "18945677"
+volume24: 10615675862.45487
+volume24a: 17074781219.82185
+*/
+
+function again(){
+    fetch(url)
+    .then(res=>res.json())
+    .then(data=>{
+        console.log(data)
+        for(var i=0; i<data.data.length; i++){
+            var names, priceOne, symbol, rank; 
+            rank = data.data[i].rank;
+            names =data.data[i].name;
+            priceOne=data.data[i].price_usd;
+            symbol = data.data[i].symbol;
+            var change1h, change24h, changeweek, changevolume;
+            change1h = data.data[i].percent_change_1h;
+            change24h = data.data[i].percent_change_24h;
+            changeweek = data.data[i].percent_change_7d;
+            changevolume = data.data[i].volume24;
+            // style
+            /*
+            1h
+            25h
+            weekly
+            volume 24
+            */
+            var cup, cdown, color1h, color24h, colorweek, colorvolume;
+            cup = '<i class="bi bi-caret-up-fill"></i>';
+            cdown = '<i class="bi bi-caret-down-fill"></i>';
+            if(change1h < 0){
+                color1h = "red";
+                change1h = cdown+change1h;
+            } else if(change1h > 0){
+                color1h = "green";
+                change1h = cup+change1h;
+            }
+
+            if(change24h < 0){
+                color24h = "red";
+                change24h = cdown+change24h;
+            } else if(change24h > 0){
+                color24h = "green";
+                change24h = cup+change24h;
+            } 
+
+            if(changeweek < 0){
+                colorweek = "red";
+                changeweek = cdown+changeweek;
+            } else if(changeweek > 0){
+                colorweek = "green";
+                changeweek = cup+changeweek;
+            } 
+            holder.innerHTML +=
+            '<div class="containers align-items-center d-flex flex-row justify-content-start p-2">'+
+            '<div class="align-items-center d-flex justify-content-center me-5 mx-3 namesheader pe-5">'+
+                '<img style="width: 25px; height: 25px;" src="'+img+data.data[i].nameid+'.png'+'" class="me-2">'+
+                '<div class="d-flex flex-column">'+
+                    '<span class="font-me p-0 p-0" style="font-size: 1rem;">'+symbol+'</span>'+
+                    '<span class="font-me p-0 p-0" style="font-size: 0.8rem;font-weight: bold;">'+names+'</span>'+
+                '</div>'+
+            '</div>'+
+            '<p class="mx-5 namesheader p-1"> <b>$</b>'+priceOne+'</p>'+
+            '<div class="align-items-center d-flex flex-nowrap my-auto">'+
+                '<p class="p-1 titlespara">'+
+                '<span style="color:'+color1h+';">'+
+                        change1h+
+                    '% </span>'+
+                '</p>'+
+                '<p class="p-1 titlespara">'+
+                '<span style="color:'+color24h+';">'+
+                        change24h+
+                    '% </span>'+
+                '</p>'+
+                '<p class="p-1 titlespara">'+
+                    '<span style="color:'+colorweek+';">'+
+                        changeweek+
+                    '% </span>'+
+                '</p>'+              
+                '<p class="p-1 titlespara">'+
+                    '<span style="color:black;"> <b>$</b>'+
+                        changevolume+
+                    '% </span>'+
+                '</p>'+
+            '</div>'+
+        '</div>'+
+        '<hr/>'
+        }
+    });
+} again()
+
+var sbutton = document.getElementById("sbutton");
+sbutton.addEventListener("click", ()=>{
+    position[0]+=position[1];
+    console.log(position)
+    again()
+})
